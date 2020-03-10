28f873bb0de5fc85e4e1ab4f3d43f79c877db3a4
Randy Tate <randytatejr@gmail.com>
2020-03-10 17:46:41 -0500

    inclass assignment

---
+++ LegoMinifigures/Composition/Heads/AstronautHead.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMinifigures.Composition.Heads
+{
+    enum LegoColor
+    {
+        Yellow,
+        Red,
+        Green,
+        White,
+        Grey,
+        Blue,
+        Black,
+        Orange
+    }
+    class AstronautHead
+    {
+        public bool Helmeted { get; set; }
+        public string FacialExpression { get; set; }
+        public LegoColor Color { get; set; }
+        public LegoColor EyeColor { get; set; }
+
+        public void Talk()
+        {
+            Console.WriteLine($"The {FacialExpression} astronaut says 'Tell my wife I love her very much.'");
+        }
+
+        public void EatPie(string typeOfPie)
+        {
+            if (typeOfPie == "apple")
+                Console.WriteLine("MMM.. Pie.");
+            else
+                Console.WriteLine("This is not pie.");
+        }
+    }
+}
---
+++ LegoMinifigures/Composition/Legs/AstronautLegs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMinifigures.Composition.Legs
+{
+    enum ShoeType
+    {
+        None,
+        Boots,
+        FlipFlops
+    }
+    class AstronautLegs
+    {
+        public bool HasPants { get; set; }
+        public ShoeType Shoes { get; set; }
+
+        Random _rng = new Random(23);
+
+        public void Walk(int steps)
+        {
+            Console.WriteLine($"The astronaut legs moonwalked {steps} steps.");
+        }
+
+        public int Jump()
+        {
+            var heightOfJump = _rng.Next();
+
+            return heightOfJump;
+        }
+    }
+}
